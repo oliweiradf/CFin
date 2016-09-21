4a6dd27c206ec2a6ee076db3e1614f0a77771aa1
oliweiradf <oliweira@hotmail.com>
2016-09-21 15:30:25 -0300

    Configurando a tela de tipo de conta fixa.

--- app/src/main/java/br/com/oliweira/cfin/TipoContaFixaActivity.java
+++ app/src/main/java/br/com/oliweira/cfin/TipoContaFixaActivity.java
@@ -1,7 +1,13 @@
 package br.com.oliweira.cfin;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteCursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
 
 public class TipoContaFixaActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class TipoContaFixaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipo_conta_fixa);
+
+        SQLiteDatabase db = openOrCreateDatabase("db_cfin",MODE_PRIVATE,null);
+        SQLiteCursor csrTipoContaFixa = (SQLiteCursor) db.rawQuery("SELECT * FROM tba_tipocontafixa",null);
+
+        ListView lvTipoContaFixa = (ListView) findViewById(R.id.lvTipoContaFixa);
+
+        String[] from = {"_id","no_tipocontafixa","tp_operador"};
+        int[] to = {R.id.tvIdTipoContaFixaLv, R.id.tvTipoContaFixaLv, R.id.tvTipoOperadorLv};
+        SimpleCursorAdapter adptTipoContaFixa = new SimpleCursorAdapter(
+                getBaseContext(),R.layout.model_lv_tipo_conta_fixa,csrTipoContaFixa,from, to, 0);
+
+        lvTipoContaFixa.setAdapter(adptTipoContaFixa);
     }
 }
